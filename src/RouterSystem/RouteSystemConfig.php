efa8b41b2574ddcd7533706904bd4253dd0bc32d
br-monteiro <bruno.monteirodg@gmail.com>
2017-06-10 15:59:45 -0300

    ref: Adicionado metodos de tratamento e configuração da requisição passada como rota

--- src/RouterSystem/RouteSystemConfig.php
+++ src/RouterSystem/RouteSystemConfig.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * Projeto de um simples encutador de URL
- * @author Edson B S MOnteiro <bruno.monteirodg@gmail.com>
- * @version 0.0.1
+ * @author Edson B S Monteiro <bruno.monteirodg@gmail.com>
+ * @version 0.0.2
  * 
  * LAUS DEO
  * 
@@ -16,10 +15,71 @@ class RouteSystemConfig
     /**
      * @var string Armazena o valor bruto da entrada de REQUEST_URI
      */
-    private $urlRaw;
+    private $uriRaw;
+
+    /**
+     * @var string Armazena o valor do verbo HTTP usado na requisiçao
+     */
+    private $method;
+
+    /**
+     *
+     * @var array Armazena o resultado da funçao explode aplicada em $this->uriRaw
+     */
+    private $arrUriDividida;
 
     public function __construct()
     {
-        
+        $this->setUriRaw()
+            ->setMethod()
+            ->trataUri();
+    }
+
+    /**
+     * Obtem o valor de REQUEST_URI
+     * @return $this
+     */
+    private function setUriRaw()
+    {
+        $this->uriRaw = $_SERVER['REQUEST_URI'];
+        return $this;
+    }
+
+    /**
+     * Retorna o valor de uriRaw
+     * @return string
+     */
+    public function getUriRaw()
+    {
+        return $this->uriRaw;
+    }
+
+    /**
+     * Obtem o valor de REQUEST_METHOD
+     * @return $this
+     */
+    private function setMethod()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        return $this;
+    }
+
+    /**
+     * Retorna o metodo da requisiçao HTTP
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    private function trataUri()
+    {
+        $this->arrUriDividida = explode('/', $this->uriRaw);
+    }
+    
+    public function getArrUriDividida()
+    {
+        return $this->arrUriDividida;
     }
 }

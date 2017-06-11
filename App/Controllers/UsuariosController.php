9fd907ba468374990681ebca6153efdf149f2cf3
br-monteiro <bruno.monteirodg@gmail.com>
2017-06-11 04:35:03 -0300

    mod: Adicionada método de criação de usuário

--- App/Controllers/UsuariosController.php
+++ App/Controllers/UsuariosController.php
@@ -5,22 +5,32 @@
  * 
  * LAUS DEO
  * 
- * Controller Teste
  */
 namespace App\Controllers;
 
 use LanceMeCore\Controller\AbstractController;
 use LanceMeCore\Interfaces\ControllerInterface;
+use App\Models\UsuariosModel;
 
-class TesteController extends AbstractController implements ControllerInterface
+class UsuariosController extends AbstractController implements ControllerInterface
 {
+
+    private $usuariosModel;
+
     public function __construct(array $parametros)
     {
         parent::__construct($parametros);
+
+        $this->usuariosModel = new UsuariosModel();
     }
 
     public function index()
     {
         dump($this->getParams('id'));
+    }
+
+    public function criar()
+    {
+        $this->usuariosModel->adicionaUsuario();
     }
 }

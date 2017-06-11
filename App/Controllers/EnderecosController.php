1a31df1c9ce38b297af2dc6b3743bf24339f8a61
br-monteiro <bruno.monteirodg@gmail.com>
2017-06-11 04:31:05 -0300

    add: Controller de Endereços

---
+++ App/Controllers/EnderecosController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Edson B S Monteiro <bruno.monteirodg@gmail.com>
+ * @version 0.0.1
+ * 
+ * LAUS DEO
+ * 
+ */
+namespace App\Controllers;
+
+use LanceMeCore\Controller\AbstractController;
+use LanceMeCore\Interfaces\ControllerInterface;
+use App\Models\EnderecosModel;
+
+class EnderecosController extends AbstractController implements ControllerInterface
+{
+
+    private $enderecosModel;
+
+    public function __construct(array $parametros)
+    {
+        parent::__construct($parametros);
+
+        $this->enderecosModel = new EnderecosModel();
+    }
+
+    public function index()
+    {
+        // todo
+    }
+
+    public function redirecionar()
+    {
+        $idUrl = $this->getParams('id');
+        $this->enderecosModel->verificaUrlPorId($idUrl);
+    }
+}
